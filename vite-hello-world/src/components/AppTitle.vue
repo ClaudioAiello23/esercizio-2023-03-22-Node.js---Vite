1d32a854fd3e36739e534f30723ab94c9dae303b
ClaudioAiello23 <claudio.aiello23@gmail.com>
2023-03-22 16:51:05 +0100

    first commit

---
+++ vite-hello-world/src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  data() {
+    return {
+
+    }
+
+  }
+}
+</script>
+
+<template>
+  <header>
+    <h1>La mia prima app con Vite!</h1>
+  </header>
+</template>
+
+<style scoped></style>
